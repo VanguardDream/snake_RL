b3b9bd01b142fc6f2fc2642f3bcf5c2f9889f279
Bongsub Song <doorebong@gmail.com>
2021-07-12 23:37:37 +0900

    joint name list split testing

--- scripts/get_state_test.py
+++ scripts/get_state_test.py
@@ -6,4 +6,6 @@ snake = mujoco_py.load_model_from_path("../description/mujoco/snake.xml")
 sim = mujoco_py.MjSim(snake)
 sim_view = mujoco_py.MjViewer(sim)
 
+joint_names = sim.model.joint_names[1:]
+
 sim_state = sim.get_state()

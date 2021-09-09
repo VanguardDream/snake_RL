44cd317328eb5e1c47848bfaceaca08fdbf7a97b
Bongsub Song <doorebong@dgist.ac.kr>
2021-09-09 15:32:20 +0900

    x,y of t testing code added

--- scripts/gait_test_lambda_gradient.py
+++ scripts/gait_test_lambda_gradient.py
@@ -37,14 +37,7 @@ def J(g, d_a, d_p, d_l, l_a, l_p, l_l, tau):
             # Commnad motor here
             simulator.data.ctrl[idx] = gen.degtorad(goal[idx])
         
-        for name in joint_names:
-            accum_theta = accum_theta + abs(simulator.data.get_joint_qpos(name))
-
         simulator.step()
-        # sim_viewer.render()
-
-        # Write step iteration state retrieve code here.
-        # s_y = appent(body_xpos('head')[1]) like this.
 
         x_of_t = np.append(x_of_t, simulator.data.get_body_xpos('head')[0])
         y_of_t = np.append(x_of_t, simulator.data.get_body_xpos('head')[1])
@@ -63,6 +56,55 @@ def J(g, d_a, d_p, d_l, l_a, l_p, l_l, tau):
 
     # print("%f : %f : %f : %f : %d : %lf" %(d_a,d_p,l_a,l_p,tau,J_value))
     return J_value
+
+
+
+def J_t(g, d_a, d_p, d_l, l_a, l_p, l_l, tau):
+    gen = gait_lambda.gait(g, d_a, d_p, d_l, l_a, l_p, l_l, int(tau))
+
+    # Variable for cost(loss) function
+    delta_x = 0
+    delta_y = 0
+    accum_theta = 0 # accumulated joint displacements for all joint.
+    x_of_t = np.array([]) # Head link x values of the time t
+    y_of_t = np.array([]) # Head link y values of the time t
+
+    # Simulation model info
+    # joint_names = simulator.model.joint_names[1:] 
+    # For generalized xml code!
+    joint_names = ['joint1','joint2','joint3','joint4','joint5','joint6','joint7','joint8','joint9','joint10','joint11','joint12','joint13','joint14']
+
+    for i in range(0,1000):
+        goal = gen.generate(i)
+
+        spec_motor = np.nonzero(goal)[0]
+
+        for idx in spec_motor:
+            # Commnad motor here
+            simulator.data.ctrl[idx] = gen.degtorad(goal[idx])
+        
+        simulator.step()
+
+        x_of_t = np.append(x_of_t, simulator.data.get_body_xpos('head')[0])
+        y_of_t = np.append(x_of_t, simulator.data.get_body_xpos('head')[1])
+
+
+    delta_x = simulator.data.get_body_xpos('head')[0]
+    delta_y = simulator.data.get_body_xpos('head')[1]
+
+    simulator.reset()
+    #Calculate Cost here
+
+    if (g == 2):
+        J_value = 3500 * abs(delta_y) - 1000 * abs(delta_x / delta_y)
+    else:
+        J_value = 1500 * delta_x - 60 * abs(delta_y) - 900 * abs(delta_y / delta_x)
+
+    # print("%f : %f : %f : %f : %d : %lf" %(d_a,d_p,l_a,l_p,tau,J_value))
+    return J_value, x_of_t, y_of_t
+
+
+
 
 def main():
     gait_type = 1
@@ -91,14 +133,20 @@ def main():
                     print('Iteration : (%d) reward : %f'  %(count,reward))
                     count = count + 1
 
-
-            
+def main2():
+    gait_type = 1
+    gait_params = [56.0, 57.7, -9.5, 71.0, 76.2, 10, 1]
     
+    # gait_type = 2
+    # gait_params = [37.2, 37.4, -8, 61.9, 61.7, 1 ,  3]
 
-    # gait_params = [36.8,    296.2,  -8, 61.3,   318.8,  1,  3] -> Side
-    # gait_params = [58.3,    344.4,  -9.4,   72.1,   193,    10.6,   1] -> Sin
+    csv_log = open('xy_plane_position_sin.csv','a')
+    csv_writer = csv.writer(csv_log)
 
+    reward, x_t, y_t = J_t(gait_type, gait_params[0], gait_params[1], gait_params[2], gait_params[33], gait_params[4], gait_params[5], gait_params[6])
 
+    csv_writer.writerow(x_t)
+    csv_writer.writerow(y_t)
 
 if __name__ == "__main__":
     while True:

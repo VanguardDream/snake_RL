968c5d717b783cc17d5d334d1f5d7dd1a453e9f6
bongsub song <dooreong@gmail.com>
2021-09-14 21:00:49 +0900

    icra2022 gait test script

--- scripts/icra/icra2022-practical.py
+++ scripts/icra/icra2022-practical.py
@@ -4,7 +4,23 @@ import time
 import gait_lambda
 import numpy as np
 from dynamixel_sdk import *
+import os
 
+if os.name == 'nt':
+    import msvcrt
+    def getch():
+        return msvcrt.getch().decode()
+else:
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    def getch():
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
 
 
 def J(g, d_a, d_p, d_l, l_a, l_p, l_l, tau):
@@ -14,35 +30,51 @@ def J(g, d_a, d_p, d_l, l_a, l_p, l_l, tau):
     delta_x = 0
     delta_y = 0
 
-    for i in range(0,100):
+    for i in range(0,1000):
         goal = gen.generate(i)
 
         spec_motor = np.nonzero(goal)[0]
 
+        commandQ = datetime.datetime.now()
         for idx in spec_motor:
             # Commnad motor here
             
             goalP = int(2048 + (goal[idx] * (1/0.088)))
             #simulator.data.ctrl[idx] = gen.degtorad(goal[idx])
 
-            # packetHandler.write4ByteTxOnly(portHandler, (idx+1), ADDR_GOAL_POSITION, goalP)
+            # GroupBW.addParam((14-(idx+1)),ADDR_GOAL_POSITION,4,goalP)
+            while(True):
+                t_period = datetime.datetime.now() - commandQ
 
-            print(idx,end=':')
-            print(goalP)
+                if t_period.microseconds > 8300 :
+                    packetHandler.write4ByteTxOnly(portHandler, (14-(idx+1)), ADDR_GOAL_POSITION, goalP)
+                    break
 
 
-            time.sleep(0.01)
 
-        # print('slot end!')
         
 
 
 def main():
-    gait_type = 1
-    gait_params = [56.0, 57.7, -9.5, 71.0, 76.2, 10, 1]
+    # gait_type = 1
+    # gait_params = [55.7, 57.2, -9.5, 70.5, 76.5, 10, 1] # Optimal
+    # gait_params = [55.7, 57.2, -9.5, 70.5, 74.5, 10, 1] # Poor minus
+    # gait_params = [55.7, 57.2, -9.5, 70.5, 78.5, 10, 1] # Poor plus
     
-    # gait_type = 2
-    # gait_params = [37.2, 37.4, -8, 61.9, 61.7, 1 ,  3]
+    gait_type = 2
+    # [37.2, 37.4, -8, 61.9, 61.7, 1 ,  3] # 옵티말
+
+    # gait_params = [38.2, 43.4, -8, 66.0, 51.6, 1 ,  3] 
+    # gait_params = [38.2, 43.4, -8, 66.0, 51.6, 1 ,  3] # Poor 원래 옵티말
+
+    # gait_params = [38.2, 41.4, -8, 66.0, 51.6, 1 ,  3] # 뉴 옵티말
+    # gait_params = [38.2, 39.4, -8, 66.0, 51.6, 1 ,  3] # Poor -
+    gait_params = [38.2, 43.4, -8, 66.0, 51.6, 1 ,  3] # Poor +
+
+    
+
+
+
 
     count = 0
 
@@ -59,22 +91,62 @@ if __name__ == "__main__":
     ADDR_TORQUE_ENABLE          = 64
     ADDR_GOAL_POSITION          = 116
     ADDR_PRESENT_POSITION       = 132
+
+    LEN_GOAL_POSITION           = 4
+
     DXL_MINIMUM_POSITION_VALUE  = 0         # Refer to the Minimum Position Limit of product eManual
     DXL_MAXIMUM_POSITION_VALUE  = 4095      # Refer to the Maximum Position Limit of product eManual
-    BAUDRATE                    = 57600 # -> 통신 속도 조절
+    BAUDRATE                    = 4000000 # -> 통신 속도 조절
 
     PROTOCOL_VERSION            = 2.0
 
     # ex) Windows: "COM*", Linux: "/dev/ttyUSB*", Mac: "/dev/tty.usbserial-*"
-    # DEVICENAME                  = '/dev/ttyUSB0'
+    DEVICENAME                  = 'COM4'
 
 
-    # portHandler = PortHandler(DEVICENAME)
-    # packetHandler = PacketHandler(PROTOCOL_VERSION)
+    # Initialize PortHandler instance
+    # Set the port path
+    # Get methods and members of PortHandlerLinux or PortHandlerWindows
+    portHandler = PortHandler(DEVICENAME)
 
-    # portHandler.openPort()
-    # portHandler.setBaudRate(BAUDRATE)
+    # Initialize PacketHandler instance
+    # Set the protocol version
+    # Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
+    packetHandler = PacketHandler(PROTOCOL_VERSION)
+
+    # Open port
+    if portHandler.openPort():
+        print("Succeeded to open the port")
+    else:
+        print("Failed to open the port")
+        print("Press any key to terminate...")
+        getch()
+        quit()
+
+
+    # Set port baudrate
+    if portHandler.setBaudRate(BAUDRATE):
+        print("Succeeded to change the baudrate")
+    else:
+        print("Failed to change the baudrate")
+        print("Press any key to terminate...")
+        getch()
+        quit()
+
+    for i in range(14):
+        packetHandler.write1ByteTxRx(portHandler, (i+1), ADDR_TORQUE_ENABLE, 1)
+
+    # GroupBW = GroupBulkWrite(portHandler,packetHandler)
 
 #모터 세팅 프로세스 끝!
+
+    print('Ready for moving! press any key to move')
+    getch()
     main()
-    print('done!')
+
+    # print('done!')
+
+    for i in range(14):
+        packetHandler.write1ByteTxRx(portHandler, (i+1), ADDR_TORQUE_ENABLE, 0)
+
+    portHandler.closePort()

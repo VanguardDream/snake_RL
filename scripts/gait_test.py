c4da1b57c2aa7bbed0e3e228e020f328d94b7ee7
bongsub song <doorebong@gmail.com>
2021-10-25 20:57:11 +0900

    plot trajectory added

--- scripts/gait_test.py
+++ scripts/gait_test.py
@@ -4,6 +4,7 @@ import random
 import gait_lambda
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
 #load model from path
 snake = mujoco_py.load_model_from_path("../description/mujoco/snake_dgist.xml")
@@ -45,8 +46,8 @@ def J(g, d_a, d_p, d_l, l_a, l_p, l_l, tau):
         # Write step iteration state retrieve code here.
         # s_y = appent(body_xpos('head')[1]) like this.
 
-        x_of_t = np.append(x_of_t, simulator.data.get_body_xpos('head')[0])
-        y_of_t = np.append(x_of_t, simulator.data.get_body_xpos('head')[1])
+        x_of_t = np.append(x_of_t, simulator.data.get_body_xpos('link6')[0])
+        y_of_t = np.append(y_of_t, simulator.data.get_body_xpos('link6')[1])
 
 
     delta_x = simulator.data.get_body_xpos('head')[0]
@@ -61,15 +62,17 @@ def J(g, d_a, d_p, d_l, l_a, l_p, l_l, tau):
         J_value = 1500 * delta_x - 60 * abs(delta_y) - 900 * abs(delta_y / delta_x)
 
     # print("%f : %f : %f : %f : %d : %lf" %(d_a,d_p,l_a,l_p,tau,J_value))
-    return J_value
+    return J_value, x_of_t, y_of_t
 
 def main():
     gait_type = 1
     
     gait_params = [55.7, 70.5,	-9.5,	57.2,	75.5,	10,	1]
-    reward = J(gait_type, gait_params[0], gait_params[1], gait_params[2], gait_params[3], gait_params[4], gait_params[5], gait_params[6])
+    reward, x_t, y_t = J(gait_type, gait_params[0], gait_params[1], gait_params[2], gait_params[3], gait_params[4], gait_params[5], gait_params[6])
 
     print('Gait\'s reward : %f'  %(reward))
+
+    plt.plot(list(range(0,1500)),x_t,y_t)
 
 if __name__ == "__main__":
     while True:

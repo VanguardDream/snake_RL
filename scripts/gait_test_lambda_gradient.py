c393cc67e1db3335565fa650ebbc59415106f593
bongsub song <doorebong@gmail.com>
2021-12-02 00:36:54 +0900

    what is chgd?

--- scripts/gait_test_lambda_gradient.py
+++ scripts/gait_test_lambda_gradient.py
@@ -20,6 +20,9 @@ def J(g, d_a, d_p, d_l, l_a, l_p, l_l, tau):
     # Variable for cost(loss) function
     delta_x = 0
     delta_y = 0
+    accum_theta = 0
+
+    joint_names = ['joint1','joint2','joint3','joint4','joint5','joint6','joint7','joint8','joint9','joint10','joint11','joint12','joint13','joint14']
 
     for i in range(0,1000):
         goal = gen.generate(i)
@@ -29,12 +32,17 @@ def J(g, d_a, d_p, d_l, l_a, l_p, l_l, tau):
         for idx in spec_motor:
             # Commnad motor here
             simulator.data.ctrl[idx] = gen.degtorad(goal[idx])
+
+        for name in joint_names:
+            accum_theta = accum_theta + abs(simulator.data.get_joint_qpos(name))
         
         simulator.step()
 
 
     delta_x = simulator.data.get_body_xpos('head')[0]
     delta_y = simulator.data.get_body_xpos('head')[1]
+
+    print('accum theta = %d' %(accum_theta))
 
     simulator.reset()
     #Calculate Cost here
@@ -209,5 +217,29 @@ def main4():
             print('Iteration : (%d) reward : %f'  %(count,reward))
             count = count + 1
 
+def main5():
+    # gait_type = 1
+    # gait_params = [55.7, 57.2, -9.5, 70.5, 76.5, 10, 1] # Optimal
+    # gait_params = [55.7, 57.2, -9.5, 70.5, 74.5, 10, 1] # Poor 2minus
+    # gait_params = [55.7, 57.2, -9.5, 70.5, 75.5, 10, 1] # Poor 1minus
+    # gait_params = [55.7, 57.2, -9.5, 70.5, 77.5, 10, 1] # Poor plus
+    # gait_params = [55.7, 57.2, -9.5, 70.5, 78.5, 10, 1] # Poor 2plus
+    
+    gait_type = 2
+    # [37.2, 37.4, -8, 61.9, 61.7, 1 ,  3] # 옵티말
+
+    # gait_params = [38.2, 43.4, -8, 66.0, 51.6, 1 ,  3] 
+    # gait_params = [38.2, 43.4, -8, 66.0, 51.6, 1 ,  3] # Poor 원래 옵티말
+
+    gait_params = [38.2, 41.4, -8, 66.0, 51.6, 1 ,  3] # 뉴 옵티말
+    # gait_params = [38.2, 39.4, -8, 66.0, 51.6, 1 ,  3] # Poor -
+    # gait_params = [38.2, 43.4, -8, 66.0, 51.6, 1 ,  3] # Poor +
+
+
+    J(gait_type, gait_params[0], gait_params[1], gait_params[2], gait_params[3], 
+                                        gait_params[4],gait_params[5],gait_params[6])
+
+
+
 if __name__ == "__main__":
-        main2()
+        main5()

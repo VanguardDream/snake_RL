65fc9d5e3c3f9c9525720acfc5afe12fc71e021b
bongusb song <doorebong@gmail.com>
2022-03-18 03:14:34 +0900

    ray tune..

--- rllib/bong-snake/raytest.py
+++ rllib/bong-snake/raytest.py
@@ -1,32 +1,14 @@
 import gym, ray
+from ray import tune
+from ray.tune.registry import register_env
 from ray.rllib.agents import ppo
+from ray.rllib.env.env_context import EnvContext
 import bongSnake
 
 ray.init()
 
-config = {
-    # Use 2 environment workers (aka "rollout workers") that parallelly
-    # collect samples from their own environment clone(s).
-    "num_workers": 1,
-    # Change this to "framework: torch", if you are using PyTorch.
-    # Also, use "framework: tf2" for tf2.x eager execution.
-    "framework": "torch",
-    # Tweak the default model provided automatically by RLlib,
-    # given the environment's observation- and action spaces.
-    "model": {
-        "fcnet_hiddens": [64, 64],
-        "fcnet_activation": "relu",
-    },
-    # Set up a separate evaluation worker set for the
-    # `trainer.evaluate()` call after training (see below).
-    "evaluation_num_workers": 1,
-    # Only for evaluation runs, render the env.
-    "evaluation_config": {
-        "render_env": True,
-    }
-}
+register_env("bongsnake", bongSnake.bongEnv)
 
-trainer = ppo.PPOTrainer(env=bongSnake.bongEnv, config = {"framework": "torch"})
+# trainer = ppo.PPOTrainer(env="bongsnake",config={"framework": "torch"})
 
-while True:
-    print(trainer.train())
+tune.run(ppo.PPOTrainer, stop= {"timesteps_total": 200}, config={"env": "bongsnake","framwork" : "torch"})

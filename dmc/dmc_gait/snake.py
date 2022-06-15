6ca823b2315c10bfdef089385cf38f39b1db15b0
bongusb song <doorebong@gmail.com>
2022-06-15 12:17:21 +0900

    20220614-dorm

--- dmc/dmc_gait/snake.py
+++ dmc/dmc_gait/snake.py
@@ -34,3 +34,23 @@ def make_model():
     #나중에 지형 변경 코드 추가 가능할 듯
 
     return etree.tostring(mjcf, pretty_print=True)
+
+class Physics(mujoco.Physics):
+    """Physics simulation with additional features for the Cartpole domain."""
+
+    def cart_position(self):
+        """Returns the position of the cart."""
+        return self.named.data.qpos['slider'][0]
+
+    def angular_vel(self):
+        """Returns the angular velocity of the pole."""
+        return self.data.qvel[1:]
+
+    def pole_angle_cosine(self):
+        """Returns the cosine of the pole angle."""
+        return self.named.data.xmat[2:, 'zz']
+
+    def bounded_position(self):
+        """Returns the state, with pole angle split into sin/cos."""
+        return np.hstack((self.cart_position(),
+                            self.named.data.xmat[2:, ['zz', 'xz']].ravel()))

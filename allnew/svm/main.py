c2de927155a1a83808c1e6c5b8a08888f98d57d7
bongusb song <doorebong@gmail.com>
2022-07-01 20:06:41 +0900

    20220701 퇴근

--- allnew/svm/main.py
+++ allnew/svm/main.py
@@ -22,7 +22,8 @@ simulator = mujoco_py.MjSim(snake)
 #Simulation Setup
 _total_time = 1680
 # _total_time = 10
-_num_iter = 10
+
+_num_iter = 10000
 
 gait_type = 1
 # gait_param = np.array([39.8, 189.9, -9.1, 66.5, 160.9, 7.0, 1]) #initial params

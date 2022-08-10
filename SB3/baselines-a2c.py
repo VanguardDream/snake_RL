40ee86a82842d1223e7ddae45f280d25aa0bb741
bongsub song <doorebong@gmail.com>
2022-08-10 21:24:22 +0900

    20220810 퇴근

--- SB3/baselines-a2c.py
+++ SB3/baselines-a2c.py
@@ -1,69 +1,70 @@
+from operator import truediv
 import os
 import gym
-import bongSnake
+import bongSnake_v3
 from stable_baselines3 import A2C
 from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
 from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.env_util import make_vec_env
 
-version = "v2"
+version = "v3"
 
-## Saving Callback func.
-class SaveCheckpoint(BaseCallback):
-	def __init__(self, save_freq, verbose = 0):
-		super(SaveCheckpoint, self).__init__(verbose)
-		self.save_freq = save_freq
+# ## Saving Callback func.
+# class SaveCheckpoint(BaseCallback):
+# 	def __init__(self, save_freq, verbose = 0):
+# 		super(SaveCheckpoint, self).__init__(verbose)
+# 		self.save_freq = save_freq
 
-	def _on_step(self):
-		if self.num_timesteps % self.save_freq == 0:
-			self.model.save("model_"+version+".zip")
-			self.training_env.save("stats_"+version+".pkl")
+# 	def _on_step(self):
+# 		if self.num_timesteps % self.save_freq == 0:
+# 			self.model.save("model_"+version+".zip")
+# 			self.training_env.save("stats_"+version+".pkl")
 
-		return True
+# 		return True
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-	# inits
-	env = gym.make('bongEnv-v2')
-	# env = Monitor(env)
+# 	# inits
+# 	env = gym.make('bongSnake-v3', render_option = True)
+# 	# env = Monitor(env)
 
-	env = DummyVecEnv([lambda: Monitor(env)])
-	model = None
+# 	env = DummyVecEnv([lambda: Monitor(env)])
+# 	model = None
 
-	# load recent checkpoint
-	if os.path.isfile("model_"+version+".zip") and os.path.isfile("stats_"+version+".pkl"):
-		env = VecNormalize.load("stats_"+version+".pkl", env)
-		env.reset()
-		model = A2C.load("model_"+version+".zip", env)
-	else:
-		env = VecNormalize(env)
-		model = A2C('MlpPolicy', env, verbose = 1, tensorboard_log = "./A2C_"+version+"/", learning_rate=0.1, gamma= 0.8)
+# 	# load recent checkpoint
+# 	if os.path.isfile("model_"+version+".zip") and os.path.isfile("stats_"+version+".pkl"):
+# 		env = VecNormalize.load("stats_"+version+".pkl", env)
+# 		env.reset()
+# 		model = A2C.load("model_"+version+".zip", env)
+# 	else:
+# 		env = VecNormalize(env)
+# 		model = A2C('MlpPolicy', env, verbose = 1, tensorboard_log = "./A2C_"+version+"/", learning_rate=0.1, gamma= 0.8)
 
-	# replay buffer
-	if os.path.isfile("replay_buffer_"+version+".pkl"):
-		model.load_replay_buffer("replay_buffer_"+version+".pkl")
+# 	# replay buffer
+# 	if os.path.isfile("replay_buffer_"+version+".pkl"):
+# 		model.load_replay_buffer("replay_buffer_"+version+".pkl")
 
-	# train
-	model.learn(500000,
-		callback = SaveCheckpoint(5000),
-		log_interval = 10,
-		reset_num_timesteps = False
-	)
+# 	# train
+# 	model.learn(500000,
+# 		callback = SaveCheckpoint(5000),
+# 		log_interval = 10,
+# 		reset_num_timesteps = False
+# 	)
 
-	env.close()
+# 	env.close()
 
 
-# # Loading & Evaluating
+# Loading & Evaluating
 
-# model = A2C.load("model_"+version")
+model = A2C.load("model_"+version+".zip")
 
-# eval_env = bongSnake.bongEnv(render_option=True)
+eval_env = bongSnake_v3.bongEnv_v3(render_option=True)
 
-# obs = eval_env.reset()
-# for i in range(1000):
-#     action, _state = model.predict(obs, deterministic=True)
-#     print(action)
-#     obs, reward, done, info = eval_env.step(action)
-#     if done:
-#       obs = eval_env.reset()
+obs = eval_env.reset()
+for i in range(10000):
+    action, _state = model.predict(obs, deterministic=True)
+    print(action)
+    obs, reward, done, info = eval_env.step(action)
+    if done:
+      obs = eval_env.reset()
--- SB3/bongSnake_v3.py
+++ SB3/bongSnake_v3.py
@@ -24,7 +24,7 @@ class bongEnv_v3(mujoco_env.MujocoEnv, utils.EzPickle):
         healthy_roll_range=(-45,45),
         healthy_pitch_range=(-30,30),
         healthy_yaw_range=(-45,45),
-        gait_params=(1, 39.8, 189.9, -9.1, 66.5, 160.9, 7.0, 1),
+        gait_params=(1, 39.8, 189.9, -9.1, 66.5, 160.9, 7.0, 10),
         render_option = False,
     ):
 
@@ -36,6 +36,8 @@ class bongEnv_v3(mujoco_env.MujocoEnv, utils.EzPickle):
         self._mujoco_exception_occured = False
 
         ## For custom env
+
+        self.initial_gait_params = gait_params
         self.gait = gait.gait(gait_params[0] 
             ,gait_params[1] 
             ,gait_params[2] 
@@ -65,7 +67,7 @@ class bongEnv_v3(mujoco_env.MujocoEnv, utils.EzPickle):
         box_min = [ 0,	0,  0, 	0, 	0, 	-10, 	0, 	0, 	-10, 	1, 	-1000, 	-1000, 	-1000, 	-100, 	-100, 	-100, 	-2, 	-2, 	-2, 	-2, 	-2, 	-2, 	-2, 	-2, 	-2, 	-2, 	-2, 	-2, 	-2, 	-2, 	-100, 	-100, 	-100, 	-100, 	-100, 	-100, 	-100, 	-100, 	-100, 	-100, 	-100, 	-100, 	-100, 	-100, 	-10, 	-10, 	-10, 	-10, 	-10, 	-10, 	-10, 	-10, 	-10, 	-10, 	-10, 	-10, 	-10, 	-10, 	-np.inf, 	-np.inf, 	-np.inf, 	-np.inf, 	-np.inf, 	-np.inf, 	-1, 	-1, 	-1, 	-1, 	-1, 	-1, 	-1, 	-1]
         box_max = [ np.inf, 10, 	1, 	90, 	360, 	10, 	90, 	360, 	10, 	6, 	1000, 	1000, 	1000, 	100, 	100, 	100, 	2, 	2, 	2, 	2, 	2, 	2, 	2, 	2, 	2, 	2, 	2, 	2, 	2, 	2, 	100, 	100, 	100, 	100, 	100, 	100, 	100, 	100, 	100, 	100, 	100, 	100, 	100, 	100, 	10, 	10, 	10, 	10, 	10, 	10, 	10, 	10, 	10, 	10, 	10, 	10, 	10, 	10, 	np.inf, 	np.inf, 	np.inf, 	np.inf, 	np.inf, 	np.inf, 	1, 	1, 	1, 	1, 	1, 	1, 	1, 	1]
 
-        self.action_space = MultiDiscrete([11,7,5,11,7,5,3])
+        self.action_space = MultiDiscrete([11,7,5,11,7,5,7])
         self.observation_space = Box(np.array(box_min),np.array(box_max), dtype=np.float32)
         
 
@@ -88,11 +90,11 @@ class bongEnv_v3(mujoco_env.MujocoEnv, utils.EzPickle):
         _before_gait_params = self.gait.get_gait_params()
 
         d_action = np.array(action)
-        d_action = d_action - np.array([6, 4, 3, 6, 4, 3, 2])
-        d_action = d_action * np.array([5, 10, 1, 5, 10, 1, 1])
+        d_action = d_action - np.array([6, 4, 3, 6, 4, 3, 4])
+        d_action = d_action * np.array([0.5, 1, 0.1, 0.5, 1, 0.1, 3])
 
         _after_gait_params = _before_gait_params + np.append([0], d_action)
-        _after_gait_params[1::] = np.clip(_after_gait_params[1::],a_min=[-85,0,-10,-85,0,-10,1], a_max=[[85,360,10,85,360,10,6]])
+        _after_gait_params[1::] = np.clip(_after_gait_params[1::],a_min=[5,0,-10,5,0,-10,1], a_max=[[85,360,10,85,360,10,200]])
 
         self.gait.setParams(_after_gait_params[0], _after_gait_params[1], _after_gait_params[2], _after_gait_params[3], _after_gait_params[4], _after_gait_params[5], _after_gait_params[6], _after_gait_params[7])
 
@@ -101,14 +103,14 @@ class bongEnv_v3(mujoco_env.MujocoEnv, utils.EzPickle):
         healthy_check = True
         state = self._get_obs()
 
-        qCoM_w, qCoM_x, qCoM_y, qCoM_z = state[68], state[69], state[70], state[71]
+        qCoM_w, qCoM_x, qCoM_y, qCoM_z = state[ENUM_OBSERVATION.quat_com_w.value], state[ENUM_OBSERVATION.quat_com_x.value], state[ENUM_OBSERVATION.quat_com_y.value], state[ENUM_OBSERVATION.quat_com_z.value]
         roll, pitch, yaw = self._quat2euler(qCoM_w, qCoM_x, qCoM_y, qCoM_z)
 
-        if abs(roll) > math.radians(60):
+        if abs(roll) > math.radians(135):
             healthy_check = False
 
-        # if abs(yaw) > math.radians(30):
-        #     healthy_check = False
+        if abs(yaw) > math.radians(40):
+            healthy_check = False
 
         if self._mujoco_exception_occured:
             healthy_check = False
@@ -121,32 +123,36 @@ class bongEnv_v3(mujoco_env.MujocoEnv, utils.EzPickle):
         return done
 
     def step(self, action):
-        obs_before = self._get_obs()
+        obs_before = self._get_obs().copy()
 
         skip_tau_scale = int(self.gait.get_gait_params()[-1])
 
         self.do_simulation(action, self.frame_skip * 14 * skip_tau_scale)
 
-        obs_after = self._get_obs()
+        obs_after = self._get_obs().copy()
 
         # later, could apply contact cost here
         ###
 
         #Reward Calculation
         x_vel, y_vel = (obs_after - obs_before)[ENUM_OBSERVATION.pos_com_x.value : ENUM_OBSERVATION.pos_com_y.value + 1]
-        forward_reward = 80 * x_vel - 80 * y_vel # 추후 계수 추가할 것
+        forward_reward = x_vel - (1.2 * abs(y_vel)) # 추후 계수 추가할 것
         healthy_reward = self.healthy_reward
-        failure_panalty = 20
+        failure_panalty = 15
+        low_tau_panalty = (15 / obs_after[ENUM_OBSERVATION.tau.value]) - 1
+        control_panalty = 0.1 * np.linalg.norm((np.array(action[:7]) - np.array([6, 4, 3, 6, 4, 3, 4])) , 1)
 
-        step_return = forward_reward + healthy_reward
+        step_return = forward_reward + healthy_reward - low_tau_panalty - control_panalty
 
         if not self.is_healthy:
             step_return = step_return - failure_panalty
 
         done = self.done
 
-        observation = obs_after
-
+        if self.render_option == True:
+            # print(self.gait.get_gait_params())
+            # print(Rot.from_quat([obs_after[ENUM_OBSERVATION.quat_com_x.value], obs_after[ENUM_OBSERVATION.quat_com_y.value], obs_after[ENUM_OBSERVATION.quat_com_z.value], obs_after[ENUM_OBSERVATION.quat_com_w.value]]).as_euler('XYZ',degrees=True))
+            pass
         info = {
             "reward_forward": forward_reward,
             "reward_contact": 0,
@@ -159,12 +165,13 @@ class bongEnv_v3(mujoco_env.MujocoEnv, utils.EzPickle):
             "forward_reward": forward_reward,
         }
 
-        return observation, step_return, done, info
+        return obs_after, step_return, done, info
 
     def _get_obs(self):
+        _sensor_data = self.sim.data.sensordata
         
-        if self.sim.data.sensordata[48:52].sum() < 0.1:
-            return  np.zeros(72)
+        # if _sensor_data[48:52].sum() < 0.1:
+        #     return  np.zeros(72)
 
         joint_names = ['joint1','joint2','joint3','joint4','joint5','joint6','joint7','joint8','joint9','joint10','joint11','joint12','joint13','joint14']
         link_names = ['head','link1','link2','link3','link4','link5','link6','link7','link8','link9','link10','link11','link12','link13','tail']
@@ -176,7 +183,7 @@ class bongEnv_v3(mujoco_env.MujocoEnv, utils.EzPickle):
         _obs_data = np.append(_obs_data, t)                                     # 1
         _obs_data = np.append(_obs_data, [self.gait.get_stride_ratio(t)])       # 1
         _obs_data = np.append(_obs_data, self.gait.get_gait_params())           # 8
-        _obs_data = np.append(_obs_data, self.sim.data.sensordata[:48])         # 48
+        _obs_data = np.append(_obs_data, _sensor_data[:48])         # 48
 
         position_head = np.array(self.sim.data.get_body_xpos('head'))
         _obs_data = np.append(_obs_data, position_head)                         # 3
@@ -184,15 +191,21 @@ class bongEnv_v3(mujoco_env.MujocoEnv, utils.EzPickle):
         position_com = np.array([self.sim.data.get_body_xpos(x) for x in link_names]).mean(axis=0)
         _obs_data = np.append(_obs_data, position_com)                          # 3
 
-        orientaion_head = np.array(self.sim.data.sensordata[-4:])
+        orientaion_head = np.array(_sensor_data[48:52])
         _obs_data = np.append(_obs_data, orientaion_head)                       # 4
 
-        orientaions_com = np.reshape(self.sim.data.sensordata[48:],(-1,4)).copy()
-        orientaions_com[:, [0, 1, 2, 3]] = orientaions_com[:, [1, 2, 3, 0]]
-        rot_com = Rot.from_quat(orientaions_com)
-        orientaion_com = rot_com.mean().as_quat()
-        orientaion_com[0], orientaion_com[1], orientaion_com[2], orientaion_com[3] = orientaion_com[3], orientaion_com[0], orientaion_com[1], orientaion_com[2]
+        orientaions_com = np.reshape(_sensor_data[48:],(-1,4)).copy()
         
+        orientaions_com[:, [0, 1, 2, 3]] = orientaions_com[:, [1, 2, 3, 0]]
+
+        try:
+            rot_com = Rot.from_quat(orientaions_com.copy())
+            orientaion_com = rot_com.mean().as_quat()
+            orientaion_com[0], orientaion_com[1], orientaion_com[2], orientaion_com[3] = orientaion_com[3], orientaion_com[0], orientaion_com[1], orientaion_com[2]
+        except:
+            print('zero quat exception occured! is initialized now?')
+            orientaion_com = np.array([0, 0, 0, 0])
+
         _obs_data = np.append(_obs_data, orientaion_com)                        # 4
 
         return _obs_data
@@ -214,6 +227,12 @@ class bongEnv_v3(mujoco_env.MujocoEnv, utils.EzPickle):
         return roll_x, pitch_y, yaw_z
 
     def reset_model(self):
+
+        if self.render_option:
+            observation = self._get_obs()
+            if (observation[ENUM_OBSERVATION.quat_com_x.value : ENUM_OBSERVATION.quat_com_x.value + 1] != [0,0,0,0]).all():
+                print(Rot.from_quat([observation[ENUM_OBSERVATION.quat_com_x.value], observation[ENUM_OBSERVATION.quat_com_y.value], observation[ENUM_OBSERVATION.quat_com_z.value], observation[ENUM_OBSERVATION.quat_com_w.value]]).as_euler('XYZ',True))
+
         self.state_k = 0
         self._mujoco_exception_occured = False
 
@@ -221,8 +240,21 @@ class bongEnv_v3(mujoco_env.MujocoEnv, utils.EzPickle):
         qvel = self.init_qvel
         self.set_state(qpos, qvel)
 
+        self.gait.setParams(self.initial_gait_params[0] 
+            ,self.initial_gait_params[1] 
+            ,self.initial_gait_params[2] 
+            ,self.initial_gait_params[3] 
+            ,self.initial_gait_params[4] 
+            ,self.initial_gait_params[5] 
+            ,self.initial_gait_params[6] 
+            ,self.initial_gait_params[7])
+
         observation = self._get_obs()
 
+        if self.render_option == True:
+            print("Reset!",end='')
+            print(self.gait.get_gait_params())
+            
         return observation
 
 

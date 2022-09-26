1758f0b4bbab143aa38e8c20de043a7bbfdb4b8a
bongsub song <dooreobng@gmail.com>
2022-09-26 18:17:51 +0900

    20220926 office

---
+++ allnew/torquebasegait/torque_gait.py
@@ -0,0 +1,11 @@
+# © 2022 Bongsub Song <doorebong@gmail.com>
+# All right reserved
+# Date : 2022-09-20 (YYYYMMDD)
+# Description : torque base gait class
+
+import math
+import numpy as np
+
+class torque_gait:
+    def __init__(self, ):
+        

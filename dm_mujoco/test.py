dc9c4c5205ce4c578894b6d3ac0f4608b8dca122
bongsub song <doorebong@gmail.com>
2023-01-05 20:55:59 +0900

    20230105 퇴근2

--- dm_mujoco/test.py
+++ dm_mujoco/test.py
@@ -4,13 +4,28 @@ import mediapy as media
 
 import numpy as np
 
+def gait_control(model : mujoco.MjModel, data : mujoco.MjData):
+    ctrl = np.random.rand(14) * 3 - 1.5
+    data.ctrl = ctrl
+
+    print(data.time)
+
+def load_callback(m=None, d=None):
+  # Clear the control callback before loading a new model
+  # or a Python exception is raised
+  mujoco.set_mjcb_control(None)
+
+  m = mujoco.MjModel.from_xml_path('./models/snake_circle_alligned.xml')
+  d = mujoco.MjData(m)
+
+  if m is not None:
+    mujoco.set_mjcb_control(lambda m, d: gait_control(m, d))
+
+  return m, d
+
 model = mujoco.MjModel.from_xml_path('./models/snake_circle_alligned.xml')
 renderer = mujoco.Renderer(model)
 data = mujoco.MjData(model)
 
-ctrl = np.array([1]*14)
-data.ctrl = ctrl
 
-viewer.launch(model,data)
-
-# print("can access over here?")
+viewer.launch(loader=load_callback)

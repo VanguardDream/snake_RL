44bacf3b29067fc9a9601c6ab6110881ac433b97
bongsub song <doorebong@gmail.com>
2023-01-05 19:10:13 +0900

    20230105 ctx testing

--- dm_mujoco/test.py
+++ dm_mujoco/test.py
@@ -6,6 +6,5 @@ model = mujoco.MjModel.from_xml_path('./models/snake_circle_alligned.xml')
 renderer = mujoco.Renderer(model)
 data = mujoco.MjData(model)
 
-mujoco.mj_forward(model, data)
-renderer.update_scene(data)
-media.show_image(renderer.render())
+ctx = mujoco.GLContext(1280, 720)
+ctx.make_current()

e9b7268db3f9e223e60a23996aa74df94794b980
bongsub song <doorebong@gmail.com>
2023-01-05 20:13:49 +0900

    20230105 퇴근

---
+++ dm_mujoco/other_examples/double_pendulum.py
@@ -0,0 +1,56 @@
+import mujoco
+import mujoco.viewer as viewer
+import numpy as np
+from scipy.linalg import solve_continuous_are
+
+# Find a K matrix for a linearized double pendulum using LQR
+def double_pendulum_lqr_K():
+  # System linearization calculated using "derivative" sample
+  A = np.array([[0, 0, 1, 0],
+                [0, 0, 0, 1],
+                [ 2.494531E+01, -1.565526E+01, -2.130544E-01,  5.164733E-01],
+                [-3.252928E+01,  6.589209E+01,  5.164733E-01, -1.793546E+00],
+               ])
+  B = np.array([[0], [0], [2.130544E+00], [-5.164733E+00]])
+
+  Q = 1 * np.array([[10,  0,  0,   0],
+                    [0, 100,  0,   0],
+                    [0,   0, 10,   0],
+                    [0,   0,  0, 100]])
+  R = 1 * np.eye(1)
+
+  S = solve_continuous_are(A, B, Q, R)
+
+  K = np.linalg.inv(R)@(B.T @ S)
+  return K
+
+def double_pendulum_control(m, d, K):
+  x = np.concatenate((
+        d.joint('shoulder').qpos,
+        d.joint('elbow').qpos,
+        d.joint('shoulder').qvel,
+        d.joint('elbow').qvel))
+
+  u = -K @ x
+  d.actuator('shoulder').ctrl[0] = u
+
+def load_callback(m=None, d=None):
+  # Clear the control callback before loading a new model
+  # or a Python exception is raised
+  mujoco.set_mjcb_control(None)
+
+  m = mujoco.MjModel.from_xml_path('./double_pendulum.xml')
+  d = mujoco.MjData(m)
+
+  if m is not None:
+    # Set some initial conditions
+    d.joint('shoulder').qpos = 0.25
+    d.joint('elbow').qpos = 0.1
+    # Set the control callback
+    K = double_pendulum_lqr_K()
+    mujoco.set_mjcb_control(lambda m, d: double_pendulum_control(m, d, K))
+
+  return m, d
+
+if __name__ == '__main__':
+  viewer.launch(loader=load_callback)
--- dm_mujoco/test.py
+++ dm_mujoco/test.py
@@ -2,9 +2,15 @@ import mujoco
 from mujoco import viewer
 import mediapy as media
 
+import numpy as np
+
 model = mujoco.MjModel.from_xml_path('./models/snake_circle_alligned.xml')
 renderer = mujoco.Renderer(model)
 data = mujoco.MjData(model)
 
-ctx = mujoco.GLContext(1280, 720)
-ctx.make_current()
+ctrl = np.array([1]*14)
+data.ctrl = ctrl
+
+viewer.launch(model,data)
+
+# print("can access over here?")

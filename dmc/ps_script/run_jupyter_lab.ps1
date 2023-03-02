98f08e897eb1c1fe3a44aa544c8171a104a62269
bongsub song <doorebong@gmail.com>
2023-03-02 10:45:24 +0900

    20230302-dorm

---
+++ dmc/ps_script/run_jupyter_lab.ps1
@@ -0,0 +1,4 @@
+conda activate rllib && jupyter lab
+
+# Create Shortcut file and move this to SHELL:STARTUP folder
+# C:\Program Files\PowerShell\7\pwsh.exe -Command "C:\Users\doore\project\snake_RL\dmc\ps_script\run_jupyter_lab.ps1"

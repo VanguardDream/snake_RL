df7bc636f5ef80d3d8aab2d71a434fb4dc348815
bongsub song <doorebong@gmail.com>
2023-03-08 11:52:53 +0900

    20230308 server configs

--- dmc/powershell_script/run_jupyter_lab.ps1
+++ dmc/powershell_script/run_jupyter_lab.ps1
@@ -1,4 +1,7 @@
 conda activate rllib && jupyter lab
 
 # Create Shortcut file and move this to SHELL:STARTUP folder
-# C:\Program Files\PowerShell\7\pwsh.exe -Command "C:\Users\doore\project\snake_RL\dmc\ps_script\run_jupyter_lab.ps1"
+# Option 1 visible pwsh window
+# "C:\Program Files\PowerShell\7\pwsh.exe" -Command "C:\Users\doore\project\snake_RL\dmc\powershell_script\run_jupyter_lab.ps1"
+# Option 2 non-visible pwsh window
+# "C:\Program Files\PowerShell\7\pwsh.exe" -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\Users\doore\project\snake_RL\dmc\powershell_script\run_jupyter_lab.ps1"

5b5bb2bd87aea15b3134f7fc19530affcc1cb66a
bongsub song <doorebong@gmail.com>
2023-03-08 10:02:48 +0900

    20230308 nightyl

--- dmc/domains/domain_test.py
+++ dmc/domains/domain_test.py
@@ -1,4 +1,16 @@
 import snake
+import numpy as np
 import gymnasium as gym
+from dm_control import viewer
 
-gym.make('snake/SnakeEnv-v1')
+env = gym.make('snake/SnakeEnv-v1',render_mode="human")
+action = gym.spaces.Box(low= -3.0, high= 3.0, shape=(14,))
+
+t = 0
+env.reset()
+while t < 1000:
+    env.step(action.sample())
+    env.render()
+    t = t + 1
+
+# env.close()
--- dmc/domains/snake/envs/SnakeEnv.py
+++ dmc/domains/snake/envs/SnakeEnv.py
@@ -23,14 +23,66 @@ class SnakeEnv(MujocoEnv, utils.EzPickle):
     "render_fps": 25,
     }
 
-    def __init__(self, frame_skip = 4) -> None:
+    def __init__(self, frame_skip = 4, **kwargs) -> None:
+        # k-th slot
+        self.k = 0
 
         # Action Space
         self.action_space = spaces.Box(low= -3.0, high= 3.0, shape=(14,))
 
         # Observation Space
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(48,)
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(49,), dtype=np.float64) # k slot and 48 sensordatas
+
+
+        MujocoEnv.__init__(self, os.path.join(__model_location__,'snake_circle_alligned.xml'), frame_skip, observation_space= self.observation_space, **kwargs)
+
+    def _get_obs(self):
+        slot = np.array(self.k)
+        sensors = self.data.sensordata[0:48]
+        observation = np.hstack((slot, sensors))
+
+        return observation
+
+    def step(self, action):
+        xy_position_before = self.data.qpos[0:2].copy()
+        self.do_simulation(action, self.frame_skip)
+        self.k = self.k + 1
+        xy_position_after = self.data.qpos[0:2].copy()
+
+        xy_velocity = (xy_position_after - xy_position_before) / self.dt
+        x_velocity, y_velocity = xy_velocity
+
+        forward_reward = x_velocity
+
+        observation = self._get_obs()
+        reward = forward_reward
+        info = {
+            "reward_fwd": forward_reward,
+            "x_position": xy_position_after[0],
+            "y_position": xy_position_after[1],
+            "distance_from_origin": np.linalg.norm(xy_position_after, ord=2),
+            "x_velocity": x_velocity,
+            "y_velocity": y_velocity,
+            "forward_reward": forward_reward,
+        }
+
+        if self.render_mode == "human":
+            self.render()
+
+        return observation, reward, False, False, info
+
+    def reset_model(self):
+        noise_low = -0.05
+        noise_high = 0.05
+
+        qpos = self.init_qpos + self.np_random.uniform(
+        low=noise_low, high=noise_high, size=self.model.nq
+        )
+        qvel = self.init_qvel + self.np_random.uniform(
+            low=noise_low, high=noise_high, size=self.model.nv
         )
 
+        self.set_state(qpos, qvel)
 
-        MujocoEnv.__init__(self, os.path.join(__model_location__,'snake_circle_alligned.xml'), frame_skip, observation_space= self.observation_space, )
+        observation = self._get_obs()
+        return observation

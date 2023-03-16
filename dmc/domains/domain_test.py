05f00fa6d2d6832571207023a13cfc936a3078db
Bongsub Song <doorebong@gmail.com>
2023-03-17 01:20:31 +0900

    20230317 nightly

--- dmc/domains/domain_test.py
+++ dmc/domains/domain_test.py
@@ -1,10 +1,10 @@
-import snake_v3
+import snake_v5
 import numpy as np
 import gymnasium as gym
 from dm_control import viewer
 
 # env = gym.make('snake/SnakeEnv-v1', render_mode="human")
-env = gym.make('snake/SnakeEnv-v3', render_mode="human")
+env = gym.make('snake/SnakeEnv-v5', render_mode="human")
 # env = gym.make('snake/SnakeEnv-v3')
 action = gym.spaces.Box(low= -3.0, high= 3.0, shape=(14,))
 
@@ -12,7 +12,8 @@ t = 0
 env.reset()
 for testing in range(3):
     for time in range(183):
-        _obs, _reward, done, _, _ = env.step(np.ones((14,)))
+        _obs, _reward, done, _, _dict = env.step(1* np.ones((14,)))
+        # print(np.argmax(_dict['head_torque']))
     env.reset()
 
 env.close()

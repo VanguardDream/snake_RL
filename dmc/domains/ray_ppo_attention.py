0392ce9e06671ce0508e65122b560a71e62ac606
bongsub song <doorebong@gmail.com>
2023-03-20 02:00:51 +0900

    20230320 ppo att

---
+++ dmc/domains/ray_ppo_attention.py
@@ -0,0 +1,30 @@
+from snake_v5.envs.SnakeEnv import SnakeEnv
+import gymnasium as gym
+
+# import rl 알고리즘
+from ray.rllib.algorithms.ppo import PPOConfig
+from ray.rllib.algorithms.algorithm_config import AlgorithmConfig
+
+from ray.tune.registry import register_env
+from ray.tune.logger import pretty_print
+
+
+register_env("snake_v5_Att", lambda config: SnakeEnv())
+
+algo = (
+    PPOConfig()
+    .rollouts(num_rollout_workers=10,)
+    .resources(num_gpus=0.95)
+    .environment(env="snake_v5_Att")
+    .framework('torch')
+    .training(gamma=0.995, lr=0.0001, clip_param=0.2, kl_coeff=1.0, num_sgd_iter=20, sgd_minibatch_size=16384, train_batch_size=320000, model= {"fcnet_hiddens": [256, 256, 128, 128, 64], "use_attention": True,}, )
+    .build()
+)
+
+for i in range(2000):
+    result = algo.train()
+    # print(pretty_print(result))
+
+    if (i + 1) % 25 == 0:
+        checkpoint_dir = algo.save()
+        print(f"Checkpoint saved in directory {checkpoint_dir}    \r")

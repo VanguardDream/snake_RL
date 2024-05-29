71cefa008f6253788fb66a520967a601694b4a61
bongsunb song <doorebong@gmail.com>
2024-05-29 18:30:22 +0900

    20240529

--- GD_tor/MCU_firmware/Two_FSR_Test/Two_FSR_Test.ino
+++ GD_tor/MCU_firmware/Two_FSR_Test/Two_FSR_Test.ino
@@ -1,26 +1,40 @@
 bool blink = false;
+unsigned long previousMillis = 0;
+
+// Apply low pass filter to up and down values
+static int prevUp = 0;
+static int prevDown = 0;
+
+float alpha = 0.6; // Adjust the filter strength (0.0 - 1.0)
+
+void lowPassFilter(int& value, int& prevValue, float alpha) {
+    value = alpha * value + (1 - alpha) * prevValue;
+    prevValue = value;
+}
 
 void setup() {
     // 초기 설정
     Serial.begin(9600);
+    pinMode(A3, INPUT);
+    pinMode(A2, INPUT);
 }
 
 void loop() {
-    int up = analogRead(A0);
-    int down = analogRead(A9);
-    if (blink) {
-        digitalWrite(LED_BUILTIN, HIGH);
+    if (millis() - previousMillis > 200) {
+        previousMillis = millis();
+        blink = !blink;
+        digitalWrite(13, blink);
+        // Serial.print("Up: ");
+        Serial.print(prevUp);
+        Serial.print(",");
+        Serial.println(prevDown);
     }
-    else {
-        digitalWrite(LED_BUILTIN, LOW);
-    }
 
-    blink = !blink;
+    int up = analogRead(A3);
+    int down = analogRead(A2);
+    
+    lowPassFilter(up, prevUp, alpha);
+    lowPassFilter(down, prevDown, alpha);
 
-    Serial.print("up: ");
-    Serial.print(up);
-    Serial.print(", down: ");
-    Serial.println(down);
-
-    delay(100);
+    delay(10);
 }

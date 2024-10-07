8d00c479890b63070367536913b10a43c79485d4
Bongsub song <doorebong@gmail.com>
2024-10-08 01:08:22 +0900

    20241008 pipe env

--- GD_tor/learning/horcrux_terrain_v1/horcrux_terrain_v1/__init__.py
+++ GD_tor/learning/horcrux_terrain_v1/horcrux_terrain_v1/__init__.py
@@ -18,6 +18,12 @@ register(
     max_episode_steps=3000,
 )
 
+register(
+    id="horcrux_terrain_v1/pipe-v1",
+    entry_point="horcrux_terrain_v1.envs:PlanePipeWorld",
+    max_episode_steps=3000,
+)
+
 # 각 지형 별로 각각 환경 등록하도록...
 # register(
 #     id="gd_tor_snake_v1/plane-control",
